6acd6bd9ad17875ef4651aa52a68051470428115
Nathan <nmcaleese87@gmail.com>
2022-08-09 11:52:32 -0700

    populate NewSubPlanPage with temperary populatedModel's state placeholders

--- src/pages/NewSubPlanPage/NewSubPlanPage.jsx
+++ src/pages/NewSubPlanPage/NewSubPlanPage.jsx
@@ -1,35 +1,49 @@
+import { useState } from "react"
 import AddExitTicketPage from "../AddExitTicketPage/AddExitTicketPage"
 import AddFirstFivePage from "../AddFirstFivePage/AddFirstFivePage"
 import AddCIPage from "../AddCIPage/AddCIPage"
 import AddLessonPlanPage from "../AddLessonPlanPage/AddLessonPlanPage"
-import { Routes, Route, Link } from 'react-router-dom'
+
+
+// function for each module:
+
+// function handleAddCIPage(){
+//         return < AddCIPage />
+//     }
+// function handleAddFirstFivePage(){
+//         return < AddFirstFivePage />
+//     }
+// function handleAddLessonPlanPage(){
+//         return < AddLessonPlanPage />
+//     }
+// function handleAddExitTicketPage(){
+//         return < AddExitTicketPage />
+//     }
+
+
+
 
 export default function NewSubPlanPage(){
-    return (
-    <div>
-       <> 
-        <h1>new sub plan page</h1>
-        {/* 
-        1. add links to module creation pages that will populate below the links on the page. 
-        2. When the links are added, they will replace the prompt in the link.
-        3. Use a ternary to determine the inner text of the link */}
-        <Link to="/ci/add">Add Classroom Instructions</Link>
-        &nbsp; | &nbsp;
-        <Link to="/first-five/add">Add First Five</Link>
-        &nbsp; | &nbsp;
-        <Link to="/lesson-plan/add">Add Lesson Plan</Link>
-        &nbsp; | &nbsp;
-        <Link to="/exit-ticket/add">Add Exit Ticket</Link>
-        <Routes>
-            <Route path='/ci/add' element={< AddCIPage />} />
-            <Route path='/first-five/add' element={< AddFirstFivePage />} />
-            <Route path='/lesson-plan/add' element={< AddLessonPlanPage />} />
-            <Route path='/exit-ticket/add' element={< AddExitTicketPage />} />
-        </Routes> 
-        </>
-        
-        
-    </div>
+//     set a constant array that has module names
+
+const modules = ['AddCIPage', 'AddFirstFivePage', 'AddLessonPlanPage', 'AddExitTicketPage']
     
+
+const [populatedModules, setPopulatedModules] = useState([`rabbit`, `rabbit`, `rabbit`, `rabbit`])
+
+
+function populatePage() {
+    console.log(`this is the modules array ${modules}`)
+    console.log(`this is the populatedModules array ${populatedModules}`)
+    populatedModules.map(module=> {
+        <h1>{module}</h1>
+    })
+}
+
+    return (
+        <div>
+            {populatedModules.map(module=> <h1>{module}</h1>)}
+        </div> 
     )
 }
+
