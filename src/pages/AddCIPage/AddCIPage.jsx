4c3d6a2004f350d360f06ccfe29e5d043acf79d6
Nathan <nmcaleese87@gmail.com>
2022-08-09 14:20:07 -0700

    minor format changes

--- src/pages/AddCIPage/AddCIPage.jsx
+++ src/pages/AddCIPage/AddCIPage.jsx
@@ -2,8 +2,11 @@ import CreateNewCIForm from '../../components/CreateNewCIForm/CreateNewCIForm'
 
 export default function AddCIPage({setPopulatedModules}){
     return (
-        <div><button onClick={() => console.log('click should load CreateNewCIForm')}>Create new Classroom Instructions Mod</button>
-        <h1>this will be a list of previous CI's</h1>
-    </div>
+        <div>
+            <button onClick={() => console.log('click should load CreateNewCIForm')}>
+                Create new Classroom Instructions Mod
+            </button>
+            <h1>this will be a list of previous CI's</h1>
+        </div>
     )
 }
