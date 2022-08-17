ee4cde8cf0eb34df8f88842d686230b6b3dc4d10
Nathan <nmcaleese87@gmail.com>
2022-08-16 19:07:50 -0700

    formatting for buttons on newsubplanpage

--- src/pages/NewSubPlanPage/NewSubPlanPage.jsx
+++ src/pages/NewSubPlanPage/NewSubPlanPage.jsx
@@ -1,115 +1,178 @@
-import { useState, useEffect } from "react"
-import * as SubPlanAPI from '../../utilities/subplan-api'
-import AddExitTicketPage from "../AddExitTicketPage/AddExitTicketPage"
-import AddFirstFivePage from "../AddFirstFivePage/AddFirstFivePage"
-import AddCIPage from "../AddCIPage/AddCIPage"
-import AddLessonPlanPage from "../AddLessonPlanPage/AddLessonPlanPage"
-import AddSubPlanNameForm from "../../components/AddSubPlanNameForm/AddSubPlanNameForm"
-import CICard from "../../components/Cards/CICard"
-import FirstFiveCard from "../../components/Cards/FirstFiveCard"
-import LessonPlanCard from "../../components/Cards/LessonPlanCard"
-import ExitTicketCard from "../../components/Cards/ExitTicketCard"
-import SubPlanNameCard from "../../components/Cards/SubPlanNameCard"
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from "react-bootstrap/button"
+import { useState, useEffect } from "react";
+import * as SubPlanAPI from "../../utilities/subplan-api";
+import AddExitTicketPage from "../AddExitTicketPage/AddExitTicketPage";
+import AddFirstFivePage from "../AddFirstFivePage/AddFirstFivePage";
+import AddCIPage from "../AddCIPage/AddCIPage";
+import AddLessonPlanPage from "../AddLessonPlanPage/AddLessonPlanPage";
+import AddSubPlanNameForm from "../../components/AddSubPlanNameForm/AddSubPlanNameForm";
+import CICard from "../../components/Cards/CICard";
+import FirstFiveCard from "../../components/Cards/FirstFiveCard";
+import LessonPlanCard from "../../components/Cards/LessonPlanCard";
+import ExitTicketCard from "../../components/Cards/ExitTicketCard";
+import SubPlanNameCard from "../../components/Cards/SubPlanNameCard";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/button";
 
-export default function NewSubPlanPage(){
+export default function NewSubPlanPage() {
+  const [populatedModules, setPopulatedModules] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
 
+  const [toggleState, setToggleState] = useState(false);
 
-const [populatedModules, setPopulatedModules] = useState([null , null , null, null, null])
+  const [activeModule, setActiveModule] = useState(null);
 
-const [toggleState, setToggleState] = useState(false)
+  const [error, setError] = useState("");
 
-const [activeModule, setActiveModule] = useState(null)
-
-const [error, setError] = useState('')
-
-const modules = [
+  const modules = [
     {
-        name: 'what do you want to call your new Sub Plan?',
-        module: <AddSubPlanNameForm setActiveModule= {setActiveModule} setPopulatedModules={setPopulatedModules} populatedModules={populatedModules}/>
+      name: "what do you want to call your new Sub Plan?",
+      module: (
+        <AddSubPlanNameForm
+          setActiveModule={setActiveModule}
+          setPopulatedModules={setPopulatedModules}
+          populatedModules={populatedModules}
+        />
+      ),
     },
     {
-        name: 'Classroom Instructions',
-        module: <AddCIPage setActiveModule= {setActiveModule} setPopulatedModules={setPopulatedModules} populatedModules={populatedModules} />,
-         
+      name: "Classroom Instructions",
+      module: (
+        <AddCIPage
+          setActiveModule={setActiveModule}
+          setPopulatedModules={setPopulatedModules}
+          populatedModules={populatedModules}
+        />
+      ),
     },
     {
-        name: 'First 5ive',
-        module: <AddFirstFivePage setActiveModule= {setActiveModule} setPopulatedModules={setPopulatedModules} populatedModules={populatedModules}  />,
+      name: "First 5ive",
+      module: (
+        <AddFirstFivePage
+          setActiveModule={setActiveModule}
+          setPopulatedModules={setPopulatedModules}
+          populatedModules={populatedModules}
+        />
+      ),
     },
     {
-        name: 'Lesson Plan',
-        module: <AddLessonPlanPage setActiveModule= {setActiveModule} setPopulatedModules={setPopulatedModules} populatedModules={populatedModules} />,
+      name: "Lesson Plan",
+      module: (
+        <AddLessonPlanPage
+          setActiveModule={setActiveModule}
+          setPopulatedModules={setPopulatedModules}
+          populatedModules={populatedModules}
+        />
+      ),
     },
     {
-        name: 'Exit Ticket',
-        module: <AddExitTicketPage setActiveModule= {setActiveModule} setPopulatedModules={setPopulatedModules} populatedModules={populatedModules}/>,  
+      name: "Exit Ticket",
+      module: (
+        <AddExitTicketPage
+          setActiveModule={setActiveModule}
+          setPopulatedModules={setPopulatedModules}
+          populatedModules={populatedModules}
+        />
+      ),
     },
-]
+  ];
 
-useEffect(function(){
-    setPopulatedModules(populatedModules)
-},[toggleState])
+  useEffect(
+    function () {
+      setPopulatedModules(populatedModules);
+    },
+    [toggleState]
+  );
 
+  const populatePage = populatedModules.map(function (module, idx) {
+    if (module && idx === 0) {
+      return (
+        <SubPlanNameCard module={module} idx={idx} removeCard={removeCard} />
+      );
+    } else if (module && idx === 1) {
+      return <CICard module={module} idx={idx} removeCard={removeCard} />;
+    } else if (module && idx === 2) {
+      return (
+        <FirstFiveCard module={module} idx={idx} removeCard={removeCard} />
+      );
+    } else if (module && idx === 3) {
+      return (
+        <LessonPlanCard module={module} idx={idx} removeCard={removeCard} />
+      );
+    } else if (module && idx === 4) {
+      return (
+        <ExitTicketCard module={module} idx={idx} removeCard={removeCard} />
+      );
+    } else {
+      return (
+        <Row>
+          <Col>
+            <Button
+              className="mt-3"
+              variant="success"
+              size="lg"
+              style={{ fontSize: 30 }}
+              onClick={() => setActiveModule(modules[idx].module)}
+            >
+              add {modules[idx].name}
+            </Button>
+          </Col>
+        </Row>
+      );
+    }
+  });
 
-const populatePage = populatedModules.map(function(module, idx){
-    if (module && idx === 0){
-        return <SubPlanNameCard module={module} idx ={idx} removeCard={removeCard}/>
-    } else if (module && idx === 1){
-        return <CICard module={module} idx ={idx} removeCard={removeCard} />
-    } else if (module && idx === 2){
-        return <FirstFiveCard module={module} idx ={idx} removeCard={removeCard}/>
-    } else if (module && idx === 3){
-        return <LessonPlanCard module={module} idx ={idx} removeCard={removeCard}/>
-    } else if (module && idx === 4){
-        return <ExitTicketCard module={module} idx ={idx} removeCard={removeCard}/>
-    } else { return <Button variant="success" size="lg" onClick={() => setActiveModule(modules[idx].module) } >add {modules[idx].name}</Button>}
-})
+  function removeCard(idx) {
+    console.log("clicked");
+    const populatedModulesCopy = populatedModules;
+    populatedModulesCopy.splice(idx, 1, null);
+    setPopulatedModules(populatedModulesCopy);
+    setToggleState(!toggleState);
+  }
 
-function removeCard(idx){
-    console.log('clicked')
-        const populatedModulesCopy = populatedModules
-        populatedModulesCopy.splice(idx, 1, null)
-        setPopulatedModules(populatedModulesCopy)
-        setToggleState(!toggleState)
-        }
+  async function createSubPlan() {
+    console.log("clicked", populatedModules);
+    try {
+      const newSubPlan = await SubPlanAPI.createSubPlan(populatedModules);
+      console.log("this is it", newSubPlan.name);
+      // handleAdd(newSubPlan);
+      setPopulatedModules([null, null, null, null, null]);
+    } catch {
+      setError("Failed to save Sub Plan");
+    }
+  }
 
-
-    async function createSubPlan() {
-        console.log('clicked', populatedModules)
-        try {
-            const newSubPlan = await SubPlanAPI.createSubPlan(populatedModules);
-            console.log('this is it', newSubPlan.name)
-            // handleAdd(newSubPlan);
-            setPopulatedModules([null , null , null, null, null])
-        } catch {
-            setError('Failed to save Sub Plan');
-        }
-        }
-
-    return (
-        <>
-            <br />
-            <div>
-                { activeModule ? 
-                    <div>
-                        {activeModule}
-                    </div>
-                :
-                    <Container>
-                        
-                        {populatePage}
-                        <Row>
-                            <Col>
-                                <Button onClick={createSubPlan} variant='success' type="submit" disabled={populatedModules.some(module => module == null)} >Create Sub Plan</Button>
-                            </Col>
-                        </Row>
-                    </Container>
-                }
-            </div>
-        </>
-    )
+  return (
+    <>
+      <br />
+      <div>
+        {activeModule ? (
+          <div>{activeModule}</div>
+        ) : (
+          <Container>
+            {populatePage}
+            <Row>
+              <Col>
+                <Button
+                  className="mt-3"
+                  onClick={createSubPlan}
+                  variant="success"
+                  type="submit"
+                  disabled={populatedModules.some((module) => module == null)}
+                >
+                  Create Sub Plan
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+        )}
+      </div>
+    </>
+  );
 }
--- src/pages/SubPlanIndexPage/SubPlanIndexPage.jsx
+++ src/pages/SubPlanIndexPage/SubPlanIndexPage.jsx
@@ -1,74 +1,78 @@
-import { useState, useEffect } from 'react'
-import SubPlaner from '../../components/SubPlan/SubPlan'
-import ViewSubPlanForm from '../../components/ViewSubPlanForm/ViewSubPlanForm';
-import * as subPlanAPI from '../../utilities/subplan-api'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button'
-
-
+import { useState, useEffect } from "react";
+import SubPlaner from "../../components/SubPlan/SubPlan";
+import ViewSubPlanForm from "../../components/ViewSubPlanForm/ViewSubPlanForm";
+import * as subPlanAPI from "../../utilities/subplan-api";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 export default function SubPlanIndexPage() {
+  const [activeSubPlan, setActiveSubPlan] = useState(null);
+  const [error, setError] = useState("");
+  const [index, setIndex] = useState([]);
 
-    const [activeSubPlan, setActiveSubPlan] = useState(null)
-    const [error, setError] = useState('')
-    const [index, setIndex] = useState([])
+  const indexSubPlans = index.map((SubPlan) => (
+    <SubPlaner
+      SubPlan={SubPlan}
+      key={SubPlan._id}
+      deleteSubPlan={deleteSubPlan}
+      viewSubPlan={viewSubPlan}
+    />
+  ));
 
-    const indexSubPlans = index.map(SubPlan => <SubPlaner SubPlan={SubPlan} key={SubPlan._id} deleteSubPlan={deleteSubPlan} viewSubPlan={viewSubPlan}/>)
-
-    useEffect( function(){
-        async function getSubPlans() {
-            const subPlans = await subPlanAPI.getAll();
-            console.log(subPlans)
-            setIndex(subPlans)
-        }
-        getSubPlans()
-    },[])
-    
-    async function viewSubPlan(SubPlan){
-        const viewSubPlan = await subPlanAPI.viewSubPlan(SubPlan._id)
-        console.log(viewSubPlan)
-        setActiveSubPlan(<ViewSubPlanForm SubPlan={viewSubPlan} deleteSubPlan={deleteSubPlan}/>)
+  useEffect(function () {
+    async function getSubPlans() {
+      const subPlans = await subPlanAPI.getAll();
+      console.log(subPlans);
+      setIndex(subPlans);
     }
+    getSubPlans();
+  }, []);
 
-    async function deleteSubPlan(SubPlan){
-        try {
-          const deletedSubPlanId = await subPlanAPI.deleteSubPlan(SubPlan)
-          handleRemove(deletedSubPlanId)
-        } catch {
-          setError('failed to delete')
-        }
-      }
+  async function viewSubPlan(SubPlan) {
+    const viewSubPlan = await subPlanAPI.viewSubPlan(SubPlan._id);
+    console.log(viewSubPlan);
+    setActiveSubPlan(
+      <ViewSubPlanForm SubPlan={viewSubPlan} deleteSubPlan={deleteSubPlan} />
+    );
+  }
 
-
-    function handleRemove(deletedSubPlanId){
-        const indexCopy = [...index]
-        const idx = indexCopy.findIndex(SubPlan => SubPlan._id === deletedSubPlanId)
-        indexCopy.splice(idx, 1)
-        setIndex(indexCopy)
-        setActiveSubPlan(null)
+  async function deleteSubPlan(SubPlan) {
+    try {
+      const deletedSubPlanId = await subPlanAPI.deleteSubPlan(SubPlan);
+      handleRemove(deletedSubPlanId);
+    } catch {
+      setError("failed to delete");
     }
+  }
 
+  function handleRemove(deletedSubPlanId) {
+    const indexCopy = [...index];
+    const idx = indexCopy.findIndex(
+      (SubPlan) => SubPlan._id === deletedSubPlanId
+    );
+    indexCopy.splice(idx, 1);
+    setIndex(indexCopy);
+    setActiveSubPlan(null);
+  }
 
-    return (
-        <Container fluid> 
-            { activeSubPlan ? 
-            <Row>
-                {activeSubPlan}
-            </Row>
-            :
-            <>
-                <Row>
-                    <Col md={{ span: 6, offset: 3 }} lg={{ span: 4, offset: 4 }}>
-                    <Button href="/subplan/new">Create new Sub Plan</Button>
-                    </Col>
-                </Row>
-                <Row xs={1} md={6} lg={6} xl={8} className="g-4">
-                    {indexSubPlans}
-                </Row>
-            </>
-            }
-        </Container>
-    )
+  return (
+    <Container fluid>
+      {activeSubPlan ? (
+        <Row>{activeSubPlan}</Row>
+      ) : (
+        <>
+          <Row>
+            <Col md={{ span: 6, offset: 3 }} lg={{ span: 4, offset: 4 }}>
+              <Button href="/subplan/new">Create new Sub Plan</Button>
+            </Col>
+          </Row>
+          <Row xs={1} md={6} lg={6} xl={8} className="g-4">
+            {indexSubPlans}
+          </Row>
+        </>
+      )}
+    </Container>
+  );
 }
